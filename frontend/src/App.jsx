d361de19d601e55fa88155c82b27ca2ea0b61309
akashmondkar7 <akashmondkar03@gmail.com>
2026-07-13 21:43:32 +0530

    google authentication done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { auth,googleProvider } from "../utils/firebase"
+import { signInWithPopup } from "firebase/auth"
+
 function App() {
+
+const googleLogin = async ()=>{
+ const data = await signInWithPopup(auth, googleProvider)
+ console.log(data)
+}
+
   return (
     <div className='w-full h-screen bg-black  flex items-center justify-center'>
-      <button className='w-50 h-24 bg-white text-black '>
+      <button className='w-50 h-24 bg-white text-black ' onClick={googleLogin}>
         continue with google
       </button>
     </div>
