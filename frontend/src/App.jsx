023c0bd806d25ca3db9fcc7316d708307155c81d
akashmondkar7 <akashmondkar03@gmail.com>
2026-07-13 21:33:58 +0530

    firebase google authantecation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className='w-full h-screen bg-black'>
-      <button className='w-50 h-24 bg-white text-black  flex items-center justify-center'>
+    <div className='w-full h-screen bg-black  flex items-center justify-center'>
+      <button className='w-50 h-24 bg-white text-black '>
         continue with google
       </button>
     </div>
---
+++ frontend/utils/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "cotexai-7fd03.firebaseapp.com",
+  projectId: "cotexai-7fd03",
+  storageBucket: "cotexai-7fd03.firebasestorage.app",
+  messagingSenderId: "751139412242",
+  appId: "1:751139412242:web:8269f458230e8d5b73fbcb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth=getAuth(app)
+export const googleProvider= new GoogleAuthProvider()
