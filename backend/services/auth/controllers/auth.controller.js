da4b1c998983279e8d3d737b426f379541220b92
akashmondkar7 <akashmondkar03@gmail.com>
2026-07-15 21:42:01 +0530

    login session logic

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -1,10 +1,35 @@
 import {} from "firebase-admin/auth"
 import App from "../../../../frontend/src/App"
+import User from "../model/user.model"
+import { createConnection } from "mongoose"
 export const login=async (req,resp) =>{
     try {
         const {token}=req.body
         
-      const  decoded=getAuth(App).verifyIdToken(token)
+      const  decoded= await getAuth(App).verifyIdToken(token)
+      const user= await User.findOne({
+    firebaseUid:decoded.uid
+
+
+      })
+      if(!user){
+        user=await User.create({
+            firebaseUid:decoded.uid,
+            name:decoded.name,
+            email:decoded.email,
+            avatar:decoded.picture
+        })
+      }
+
+    const sessionId=crypto.randomUUID()
+
+    resp.cookie("session",sessionId,{
+        httpOnly:true,
+        secure:false,
+        sameSite:"strict",
+        maxAge:7*24*60*60*1000
+    })
+
     } catch (error) {
         
     }
