7ee6dec76da8c08c27ff7f3faa153ffa69fdc195
akashmondkar7 <akashmondkar03@gmail.com>
2026-07-17 11:31:32 +0530

    sync req

--- backend/getway/index.js
+++ backend/getway/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import proxy from "express-http-proxy";
-
+import cors from "cors"
 dotenv.config();
 
 const port = process.env.PORT || 8000;
 const app = express();
+
+app.use(cors({
+  origin:process.env.FRONTEND_URL,
+  credentials:true
+}))
 
 app.use("/auth",proxy(process.env.AUTH_SERVICE))
 app.get("/",(req,resp)=>{
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
 import { auth,googleProvider } from "../utils/firebase"
 import { signInWithPopup } from "firebase/auth"
+import api from "../utils/axios.js"
 
 function App() {
 
+  const handleLogin=async(token)=>{
+    try{
+      const {data}= await api.post("/auth/login",token)
+      console.log(data)
+
+    }catch(error){
+      console.error("Error during login:", error)
+    }
+  }
+
 const googleLogin = async ()=>{
  const data = await signInWithPopup(auth, googleProvider)
- 
+ const token = await data.user.getIdToken()
+ console.log(token);
+ await handleLogin(token)
  console.log(data)
 }
 
